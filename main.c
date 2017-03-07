3b1462423c8cb058e780a9cc862f8ce3c2bf36ad
Vilaaze <brian@reeder.com>
2017-03-06 23:27:02 -0600

    v0.0.0 Hello World Testing local gcc compiler

---
+++ main.c
@@ -0,0 +1,16 @@
+//// //// //// //// //// ////
+//
+// Brian John Sedwick Reeder
+// reeder.brianj@gmail.com
+//
+// End-To-End Encryption Server
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
