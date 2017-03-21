94c062b55462fd9df01b2fdccc1d3f71a1e9d0dc
Vilaaze <brian@reeder.com>
2017-03-21 13:50:30 -0500

    v0.1.2 AES - Plain Text Buffer Plaintext is retrieved from user Plain text may span multiple lines ctrl + D closes the input scanner

--- client/client.c
+++ client/client.c
@@ -17,17 +17,31 @@
 
 int main()
 {
+    // Client/Server Vars
     int portno, sockfd, n;
-    char msg[80];
+    char msg[256] = {0};
     portno = 8080;
 
-
+    // Encryption Vars
     unsigned char* key;
-    printf("[CLIENT]: Key: ");
+    char buffer[256] = {0};
+
+    // Establish Key for AES
+    printf("[CLIENT]: Key = ");
     key = new_key();
     for(n = 0; n < 32; n++)
         printf("%x ", key[n]);
     printf("\n\n");
+
+    // Retrieve Plaintext from user
+    printf("[CLIENT]: Enter the Plain text to be encrypted [^D to end message]:\n>> ");
+    while(fgets(buffer, 256, stdin) != NULL && printf(">> "))
+        strncat(msg, buffer, 256);
+    printf("^D\n\n");
+   
+    // Strip trailing newline from input
+    msg[strlen(msg)-1] = '\0';
+    printf("%s\n\n", msg); 
 
     // Establish Connection to server
     printf("[CLIENT]: Establishing connection with server.\n");
@@ -41,15 +55,16 @@ int main()
     {
         printf("[CLIENT]: ERROR writing to socket\nTerminating...\n");
         close(sockfd);
-        exit(-1);
+        exit(1);
     }
-
+    
+    // Recieve Response From Server
     n = read(sockfd, msg, sizeof(char) * 80);
     if(n < 0)
     {
         printf("[SYSTEM]: ERROR reading from socket.\nTeminating...\n");
         close(sockfd);
-        exit(-1);
+        exit(1);
     }
     printf("[SERVER]: %s\n", msg);
 
@@ -57,5 +72,6 @@ int main()
     printf("[CLIENT]: Closing socket.\n");
     close(sockfd);
 
+    free(key);
     return 0;
 }
